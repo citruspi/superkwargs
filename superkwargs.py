b39e5a6e0afe13f76e400b893ef8f311fa5b0f6e
Mihir Singh <git.service@mihirsingh.com>
2017-08-24 20:34:59 -0400

    Added support for default kwarg values

--- superkwargs.py
+++ superkwargs.py
@@ -10,13 +10,16 @@ class MissingRequiredKwargException(SuperkwargException):
     pass
 
 
-def kwarg(name, required=False):    
+def kwarg(name, required=False, default=None):
     def decorator(function):
         def superkwarg(*args, **kwargs):
             if required and name not in kwargs:
                 raise MissingRequiredKwargException(
                     'Keyword argument \'{arg}\' required to invoke \'{func}\''.format(
                     arg=name, func=function.__name__))
+
+            if name not in kwargs:
+               kwargs[name] = default
 
             return function(*args, **kwargs)
         return superkwarg
