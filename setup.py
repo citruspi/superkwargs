7add9323ed461d74070e97d1e65b88ccb0eeb0fd
Mihir Singh <git.service@mihirsingh.com>
2017-09-05 18:03:00 -0400

    Bumped the version to 0.3.0

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='superkwargs',
-    version='0.2.0',
+    version='0.3.0',
     author='Mihir Singh (@citruspi)',
     author_email='hello@mihirsingh.com',
-    packages=find_packages(),    
+    packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
